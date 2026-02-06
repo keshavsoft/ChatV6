f98961baf4e47af78ba8c4ad13f5a46526813dfa
keshavsoft <nknnkumar@yahoo.com>
2026-02-06 11:23:22 +0530

    v1 ready for copy

--- app/src/main/java/com/example/compose/jetchat/websocket/v1/ConversationFragment.kt
+++ app/src/main/java/com/example/compose/jetchat/websocket/v1/ConversationFragment.kt
@@ -1,50 +1,35 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.example.compose.jetchat.websocket.v1.conversation
+package com.example.compose.jetchat.websocket.v1
 
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.ComposeView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
+import com.example.compose.jetchat.AppWebSocketManager
 import com.example.compose.jetchat.MainViewModel
 import com.example.compose.jetchat.R
+import com.example.compose.jetchat.theme.JetchatTheme
+import com.example.compose.jetchat.websocket.common.conversation.ConversationContent
 import com.example.compose.jetchat.websocket.common.conversation.ConversationUiState
 import com.example.compose.jetchat.websocket.common.conversation.Message
-import com.example.compose.jetchat.theme.JetchatTheme
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateListOf
-import com.example.compose.jetchat.AppWebSocketManager
-import com.example.compose.jetchat.websocket.common.conversation.ConversationContent
 
 class ConversationFragment : Fragment() {
     private val activityViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
         ComposeView(inflater.context).apply {
-            layoutParams = LayoutParams(MATCH_PARENT, MATCH_PARENT)
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
 
             setContent {
                 // 1. Local UI messages
